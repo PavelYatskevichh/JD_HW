9b8ab4226bdbe66c1b40c3280aafa37a51277351
Pavel Yatskevich <yatskevich199902@gmail.com>
2023-05-20 02:29:08 +0300

    Lesson 10.

---
+++ src/com/academy/yatskevich/lesson10/Task1.java
@@ -0,0 +1,17 @@
+package com.academy.yatskevich.lesson10;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String s = null;
+        try {
+            if (Math.random() > 0.5) {
+                s.length();
+            } else {
+                Integer.parseInt(s);
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getClass());
+        }
+
+    }
+}
